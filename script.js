17b60c331dd053bf1da0e6d4ed7f4bff99e545af
Deepabisht231 <d231bisht@gmail.com>
2024-02-22 20:46:21 +0530

    confirm

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const images = [
+    { "image": "http://hdqwalls.com/wallpapers/colorful-parrot-bird.jpg" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+    { "image": "https://via.placeholder.com/150" },
+];
